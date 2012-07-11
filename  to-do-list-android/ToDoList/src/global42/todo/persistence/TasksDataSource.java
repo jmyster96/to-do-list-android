bf1cc00d46fbaf71429e0295f695aa8b47190a39
dominik.joder <dominik.joder@gmail.com>
2012-07-11 15:00:20 +0000

    stub for getTasksSorted(sorting, boolean showDoneItems)

---  to-do-list-android/ToDoList/src/global42/todo/persistence/TasksDataSource.java
+++  to-do-list-android/ToDoList/src/global42/todo/persistence/TasksDataSource.java
@@ -78,6 +78,10 @@ public class TasksDataSource implements Serializable {
 	}
 	
 	public List<Task> getTasksSorted(ListSorting sorting) {
+		return getTasksSorted(sorting, true)
+	}
+	
+	public List<Task> getTasksSorted(ListSorting sorting, boolean showDoneItems) {
 		Cursor returnedCursor;
 		switch (sorting) {
 			case Priority:
